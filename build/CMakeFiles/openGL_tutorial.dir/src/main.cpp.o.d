8e3d6a3b539a9dcbc7c04ca53ad58465a81f5269
Dane Dalton <danezdalton1@gmail.com>
2025-03-20 22:00:28 -0500

    Include GLFW, open a window.

--- build/CMakeFiles/openGL_tutorial.dir/src/main.cpp.o.d
+++ build/CMakeFiles/openGL_tutorial.dir/src/main.cpp.o.d
@@ -149,4 +149,8 @@ CMakeFiles/openGL_tutorial.dir/src/main.cpp.o: \
  /usr/include/c++/13/bits/locale_facets.tcc \
  /usr/include/c++/13/bits/basic_ios.tcc \
  /usr/include/c++/13/bits/ostream.tcc /usr/include/c++/13/istream \
- /usr/include/c++/13/bits/istream.tcc
+ /usr/include/c++/13/bits/istream.tcc /usr/include/GLFW/glfw3.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h /usr/include/GL/gl.h \
+ /usr/include/GL/glext.h /usr/include/KHR/khrplatform.h
