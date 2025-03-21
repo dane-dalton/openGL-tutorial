a6ad2ac02d424c4e0ca2a55316e6ea85cdd8b3fe
Dane Dalton <danezdalton1@gmail.com>
2025-03-20 20:54:21 -0500

    Init with CMake

---
+++ build/CMakeFiles/openGL_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openGL_tutorial.dir/src/main.cpp.o"
+  "CMakeFiles/openGL_tutorial.dir/src/main.cpp.o.d"
+  "openGL_tutorial"
+  "openGL_tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openGL_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
