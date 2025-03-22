e9a80c9da8499cd165e49452fac12d1b8ad3306d
Dane Dalton <danezdalton1@gmail.com>
2025-03-22 08:07:43 -0500

    Add vertex of triangle positions, create buffer, define buffer, point to buffer and describe the shape of data, then point to buffer and draw

--- src/main.cpp
+++ src/main.cpp
@@ -21,16 +21,30 @@ int main(int, char**) {
 
   glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
 
+  std::cout << glGetString(GL_VERSION) << std::endl;
+
+  float positions[6] = {
+    -0.5f, -0.5f,
+    0.0f, 0.5f,
+    0.5f, -0.5f
+  };
+
+  unsigned int buffer;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
 
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glBegin(GL_TRIANGLES);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f(0.0f, 0.5f);
-    glVertex2f(0.5f, -0.5f);
-    glEnd();
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
   }
